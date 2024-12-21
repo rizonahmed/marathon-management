30b94ea25fa0642fdc861bc11f6809e5f2ed31c6
rizonahmed <rizonahmed0486@gmail.com>
2024-12-22 00:25:30 +0600

    completed the basic setup of clint side

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ import { Outlet } from 'react-router-dom'
+import './App.css'
+import Nav from './Nav'
+import Footer from './Footer'
+
+function App() {
+  
+  return (
+    <div>
+      <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Nav from './Nav.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      // {
+      //   path: '/nav',
+      //   element: <Nav></Nav>
+      // }
+     
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
